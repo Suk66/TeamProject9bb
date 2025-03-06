d14dcd535ac1057cded3434b655600463ed2dd35
Suk66 <goawwl66@gmail.com>
2025-03-06 17:03:16 +0900

    to 준우

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO admin (admin_name, admin_pwd, admin_email, admin_phone, role) VALUES
+                                                                              ('kim1', '3BA25A9F45DCEEE9464AA7B11A5E91D71E16D172AF2DAA1DE5BF01FAF483604E', 'kim1@google.com', '010-1111-1111', 'ADMIN'),
+                                                                              ('kim2', '3BA25A9F45DCEEE9464AA7B11A5E91D71E16D172AF2DAA1DE5BF01FAF483604E', 'kim2@google.com', '010-2222-2222', 'ADMIN'),
+                                                                              ('kim3', '3BA25A9F45DCEEE9464AA7B11A5E91D71E16D172AF2DAA1DE5BF01FAF483604E', 'kim3@google.com', '010-3333-3333', 'ADMIN'),
+                                                                              ('kim4', '3BA25A9F45DCEEE9464AA7B11A5E91D71E16D172AF2DAA1DE5BF01FAF483604E', 'kim4@google.com', '010-4444-4444', 'ADMIN'),
+                                                                              ('kim5', '3BA25A9F45DCEEE9464AA7B11A5E91D71E16D172AF2DAA1DE5BF01FAF483604E', 'kim5@google.com', '010-5555-5555', 'ADMIN');
+
+INSERT INTO customer (customer_name, customer_pwd, customer_email, customer_phone, customer_addr, role) VALUES
+                                                                                                            ('lee1', '3BA25A9F45DCEEE9464AA7B11A5E91D71E16D172AF2DAA1DE5BF01FAF483604E', 'lee1@gmail.com', '010-6666-6666', 'Seoul, Korea', 'CUSTOMER'),
+                                                                                                            ('lee2', '3BA25A9F45DCEEE9464AA7B11A5E91D71E16D172AF2DAA1DE5BF01FAF483604E', 'lee2@gmail.com', '010-7777-7777', 'Busan, Korea', 'CUSTOMER'),
+                                                                                                            ('lee3', '3BA25A9F45DCEEE9464AA7B11A5E91D71E16D172AF2DAA1DE5BF01FAF483604E', 'lee3@gmail.com', '010-8888-8888', 'Daegu, Korea', 'CUSTOMER'),
+                                                                                                            ('lee4', '3BA25A9F45DCEEE9464AA7B11A5E91D71E16D172AF2DAA1DE5BF01FAF483604E', 'lee4@gmail.com', '010-9999-9999', 'Incheon, Korea', 'CUSTOMER'),
+                                                                                                            ('lee5', '3BA25A9F45DCEEE9464AA7B11A5E91D71E16D172AF2DAA1DE5BF01FAF483604E', 'lee5@gmail.com', '010-1010-1010', 'Daejeon, Korea', 'CUSTOMER');
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,27 +1,32 @@
--- 관리자 상세 정보
 create table admin(
-    admin_id int auto_increment primary key,
-    admin_name varchar(50) not null,
-    admin_pwd varchar(255) not null,
-    admin_email varchar(100) unique not null,
-    admin_phone varchar(20) unique not null
+                      admin_id int auto_increment primary key,
+                      admin_name varchar(50) not null,
+                      admin_pwd varchar(255) not null,
+                      admin_email varchar(100) unique not null,
+                      admin_phone varchar(20) unique not null,
+                      role ENUM('ADMIN') NOT NULL DEFAULT 'ADMIN'
 );
 
 -- 회원 상세 정보
 create table customer(
-    customer_id int auto_increment primary key,
-    customer_name varchar(50) not null,
-    customer_pwd varchar(255) not null,
-    customer_email varchar(100) unique not null,
-    customer_phone varchar(20) unique not null,
-    customer_addr varchar(255) not null,
-    created_at timestamp default current_timestamp
+                         customer_id int auto_increment primary key,
+                         customer_name varchar(50) not null,
+                         customer_pwd varchar(255) not null,
+                         customer_email varchar(100) unique not null,
+                         customer_phone varchar(20) unique not null,
+                         customer_addr varchar(255) not null,
+                         created_at timestamp default current_timestamp,
+                         role ENUM('CUSTOMER') NOT NULL DEFAULT 'CUSTOMER'
 );
 
--- 제품 정보
-create table product(
-    product_id int auto_increment primary key,
-    product_name varchar(100) not null,
-    description text,
-    product_price decimal(10,2) not null
+CREATE TABLE product (
+                         product_id INT AUTO_INCREMENT PRIMARY KEY,
+                         product_name VARCHAR(100) NOT NULL,
+                         description TEXT,
+                         product_price DECIMAL(10,2) NOT NULL CHECK (product_price >= 0),
+                         phone_category INT,
+                         image_url varchar(255),
+                         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+use ihwastack;
