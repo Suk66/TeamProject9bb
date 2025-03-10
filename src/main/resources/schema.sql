e9ec539d49883835946ddf7c9915c23840dca5d4
Suk66 <goawwl66@gmail.com>
2025-03-10 13:56:35 +0900

    3월10일자 오전작업

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,24 +9,24 @@ create table admin(
 
 -- 회원 상세 정보
 create table customer(
-                         customer_id int auto_increment primary key,
-                         customer_name varchar(50) not null,
-                         customer_pwd varchar(255) not null,
-                         customer_email varchar(100) unique not null,
-                         customer_phone varchar(20) unique not null,
-                         customer_addr varchar(255) not null,
-                         created_at timestamp default current_timestamp,
-                         role ENUM('CUSTOMER') NOT NULL DEFAULT 'CUSTOMER'
+         customer_id int auto_increment primary key,
+         customer_name varchar(50) not null,
+         customer_pwd varchar(255) not null,
+         customer_email varchar(100) unique not null,
+         customer_phone varchar(20) unique not null,
+         customer_addr varchar(255) not null,
+         created_at timestamp default current_timestamp,
+         role ENUM('CUSTOMER') NOT NULL DEFAULT 'CUSTOMER'
 );
 
 CREATE TABLE product (
-                         product_id INT AUTO_INCREMENT PRIMARY KEY,
-                         product_name VARCHAR(100) NOT NULL,
-                         description TEXT,
-                         product_price DECIMAL(10,2) NOT NULL CHECK (product_price >= 0),
-                         phone_category INT,
-                         image_url varchar(255),
-                         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+         product_id INT AUTO_INCREMENT PRIMARY KEY,
+         product_name VARCHAR(100) NOT NULL,
+         description varchar(1000),
+         product_price DECIMAL(10,2) NOT NULL CHECK (product_price >= 0),
+         phone_category INT,
+         image_path varchar(500),
+         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 use ihwastack;
