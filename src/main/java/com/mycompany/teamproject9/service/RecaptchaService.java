b9606e71e479801c71030d60042260a60e7b5d43
Suk66 <goawwl66@gmail.com>
2025-03-13 17:45:43 +0900

    자장면

--- src/main/java/com/mycompany/teamproject9/service/RecaptchaService.java
+++ src/main/java/com/mycompany/teamproject9/service/RecaptchaService.java
@@ -1,6 +1,7 @@
 package com.mycompany.teamproject9.service;
 
 import com.mycompany.teamproject9.config.RecaptchaConfig;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -8,17 +9,16 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
+@AllArgsConstructor
 @Service
 public class RecaptchaService {
-
     private final RecaptchaConfig recaptchaConfig;
     private final RestTemplate restTemplate;
 
-    public RecaptchaService(RecaptchaConfig recaptchaConfig, RestTemplate restTemplate, RestTemplate restTemplate1) {
-        this.recaptchaConfig = recaptchaConfig;
-        this.restTemplate = restTemplate1;
-    }
+//    public RecaptchaService(RecaptchaConfig recaptchaConfig, RestTemplate restTemplate, RestTemplate restTemplate1) {
+//        this.recaptchaConfig = recaptchaConfig;
+//        this.restTemplate = restTemplate1;
+//    }
 //    @Value("${recaptcha.site-key}")
 //    private String siteKey;
 //
@@ -30,7 +30,7 @@ public class RecaptchaService {
         String url = "https://www.google.com/recaptcha/api/siteverify";
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("secret", RecaptchaConfig.getSecreKey());
+        params.add("secret", recaptchaConfig.getSecretKey());
         params.add("response", recaptchaResponse);
 
         // Google reCAPTCHA 검증 API에 요청
